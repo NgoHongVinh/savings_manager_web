9ae8e423bc31f3eaba36c6006f903f69b39f0eaa
vietng <vietng322611@gmail.com>
2026-04-23 08:26:58 +0700

    update savings model

--- apps/savings/models.py
+++ apps/savings/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Savings(models.Model):
+    type = models.CharField(max_length=20)
+    citizen_id = models.CharField(max_length=12)
+    address = models.CharField(max_length=100)
+    date = models.DateField()
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+
+    user = models.ForeignKey('users.CustomUser', on_delete=models.CASCADE)
