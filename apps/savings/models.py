a407a45a6fe98e60c6f4012b1dddb9ccb2e50390
vietng <vietng322611@gmail.com>
2026-04-23 08:55:42 +0700

    add `account_number` primary key to `SavingAccount` model

--- apps/savings/models.py
+++ apps/savings/models.py
@@ -1,13 +1,24 @@
 from django.db import models
 from django.conf import settings
+import random
+
+def generate_account_number():
+    """Generates a random 10-digit account number."""
+    return "".join([str(random.randint(0, 9)) for _ in range(10)])
 
 class SavingAccount(models.Model):
+    account_number = models.CharField(
+        primary_key=True, 
+        max_length=10, 
+        default=generate_account_number, 
+        editable=False
+    )
     type = models.CharField(max_length=20)
-    name = models.CharField(max_length=50)
+    name = models.CharField(max_length=50) # account holder's name'
     citizen_id = models.CharField(max_length=12)
     address = models.CharField(max_length=100)
-    created_at = models.DateTimeField(auto_now_add=True)
     balance = models.DecimalField(max_digits=12, decimal_places=2, default=0.00)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='saving_accounts')
 
@@ -18,11 +29,13 @@ class Transaction(models.Model):
         ('WITHDRAW', 'Withdrawal'),
     ]
 
-    account = models.ForeignKey(SavingAccount, on_delete=models.CASCADE, related_name='transactions')
+    account_number = models.CharField(max_length=10)
+    name = models.CharField(max_length=50) # transaction maker's name
     transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     timestamp = models.DateTimeField(auto_now_add=True)
-    description = models.TextField(blank=True, null=True)
+
+    account = models.ForeignKey(SavingAccount, on_delete=models.CASCADE, related_name='transactions')
 
     def __str__(self):
         return f"{self.transaction_type}: {self.amount} on {self.timestamp}"
