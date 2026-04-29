c31f23a41d12f96548101d71800c937039dac5bb
vietng <vietng322611@gmail.com>
2026-04-29 18:47:16 +0700

    implement admin page for `savings`

--- savings/admin.py
+++ savings/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from models import *
 
-# Register your models here.
+@admin.register(SavingType)
+class SavingAdmin(admin.ModelAdmin):
+    list_display = ("name", "duration_months", "interest_rate", "is_active")
+
+@admin.register(SavingAccount)
+class SavingAdmin(admin.ModelAdmin):
+    list_display = ("user", "type", "name", "citizen_id", "address", "balance", "created_at")
+
+@admin.register(Transaction)
+class SavingAdmin(admin.ModelAdmin):
+    list_display = ("account_number", "name", "transaction_type", "amount", "timestamp")
