4ee8fc7c79b03b7e202a4aa5243a3d74c0d28ff2
vietng <vietng322611@gmail.com>
2026-05-20 19:00:30 +0700

    add information change form

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,44 +2,53 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import PasswordChangeForm
 from django.contrib.auth import update_session_auth_hash
-from .forms import EmailChangeForm
+from users.forms import InformationChangeForm, EmailChangeForm
 from django.contrib import messages
 
 from .utils import read_session_errors
 
-
 @login_required
 def profile(request):
     if request.method == "POST":
-        if request.POST.get("form_type") == "password":
-            password_form = PasswordChangeForm(request.user, request.POST)
+        match request.POST.get("form_type"):
+            case "information":
+                information_form = InformationChangeForm(request.POST, instance=request.user)
+                if information_form.is_valid():
+                    information_form.save()
+                    messages.success(request, "Information updated successfully.")
+                else:
+                    request.session["info_errors"] = information_form.errors
+            case "password":
+                password_form = PasswordChangeForm(request.user, request.POST)
 
-            if password_form.is_valid():
-                user = password_form.save()
-                messages.success(request, "Password updated successfully.")
-                update_session_auth_hash(request, user)
-            else:
-                request.session["password_errors"] = password_form.errors
+                if password_form.is_valid():
+                    user = password_form.save()
+                    messages.success(request, "Password updated successfully.")
+                    update_session_auth_hash(request, user)
+                else:
+                    request.session["password_errors"] = password_form.errors
+            case "email":
+                email_form = EmailChangeForm(request.POST, user=request.user)
 
-        elif request.POST.get("form_type") == "email":
-            email_form = EmailChangeForm(request.POST, user=request.user)
-
-            if email_form.is_valid():
-                email_form.save()
-                messages.success(request, "Email updated successfully.")
-            else:
-                request.session["email_errors"] = email_form.errors
+                if email_form.is_valid():
+                    email_form.save()
+                    messages.success(request, "Email updated successfully.")
+                else:
+                    request.session["email_errors"] = email_form.errors
 
         return redirect("profile")
 
     else:
+        information_form = InformationChangeForm(instance=request.user)
         password_form = PasswordChangeForm(request.user)
         email_form = EmailChangeForm(user=request.user)
 
+        read_session_errors(information_form, request.session, "info_errors")
         read_session_errors(password_form, request.session, "password_errors")
         read_session_errors(email_form, request.session, "email_errors")
 
     return render(request, "account/profile.html", {
+        "information_form": information_form,
         "password_form": password_form,
         "email_form": email_form,
     })
