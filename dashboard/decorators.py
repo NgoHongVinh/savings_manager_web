ea3d75e27e5c19b8d611dd73dd93557a267abf5d
vietng <vietng322611@gmail.com>
2026-05-21 13:31:36 +0700

    fix redirect paths

--- dashboard/decorators.py
+++ dashboard/decorators.py
@@ -3,10 +3,10 @@ from django.shortcuts import redirect
 def customer_required(view_func):
     def wrapper(request, *args, **kwargs):
         if not request.user.is_authenticated:
-            return redirect("login")
+            return redirect("/login")
 
         if not hasattr(request.user, "customer"):
-            return redirect("admin")
+            return redirect("/admin")
 
         return view_func(request, *args, **kwargs)
 
@@ -15,10 +15,10 @@ def customer_required(view_func):
 def employee_required(view_func):
     def wrapper(request, *args, **kwargs):
         if not request.user.is_authenticated:
-            return redirect("login")
+            return redirect("/login")
 
         if not hasattr(request.user, "employee"):
-            return redirect("profile")
+            return redirect("/profile")
 
         return view_func(request, *args, **kwargs)
 
