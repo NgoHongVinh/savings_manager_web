f59c61add560f0b495b6473033b1702e85ab1497
vietng <vietng322611@gmail.com>
2026-05-21 13:55:54 +0700

    add script to create initial superuser + modify admin page

--- create_superuser.py
+++ create_superuser.py
@@ -1,10 +1,11 @@
 import os
 import django
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "dashboard.settings")
 django.setup()
 
 from django.contrib.auth import get_user_model
+from allauth.account.models import EmailAddress
 
 User = get_user_model()
 
@@ -12,10 +13,18 @@ email = os.getenv("DJANGO_SUPERUSER_EMAIL")
 password = os.getenv("DJANGO_SUPERUSER_PASSWORD")
 
 if not User.objects.filter(email=email).exists():
-    User.objects.create_superuser(
+    user = User.objects.create_superuser(
         email=email,
         password=password
     )
+
+    EmailAddress.objects.create(
+        user=user,
+        email=user.email,
+        primary=True,
+        verified=True
+    )
+
     print("Superuser created")
 else:
     print("Superuser already exists")
--- savings/admin.py
+++ savings/admin.py
@@ -1,20 +1,7 @@
 from django.contrib import admin
 from .models import *
 
-@admin.register(SavingType)
-class SavingAdmin(admin.ModelAdmin):
-    list_display = ("name", "duration_months", "interest_rate", "is_active", "is_flexible")
-
-@admin.register(SavingAccount)
-class SavingAdmin(admin.ModelAdmin):
-    list_display = ("saving_type", "balance", "created_at", "interest_rate", "start_date", "maturity_date", "interest_last_applied_on")
-    list_select_related = ("user", "saving_type")
-
-@admin.register(SavingTypeRateHistory)
-class SavingRateHistoryAdmin(admin.ModelAdmin):
-    list_display = ("saving_type", "interest_rate", "effective_from", "effective_to")
-
-@admin.register(Transaction)
-class SavingAdmin(admin.ModelAdmin):
-    list_display = ("transaction_type", "balance_before", "amount", "balance_after", "timestamp")
-    list_select_related = ("account", "user")
+admin.site.register(SavingType)
+admin.site.register(SavingAccount)
+admin.site.register(SavingTypeRateHistory)
+admin.site.register(Transaction)
