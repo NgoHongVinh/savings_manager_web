826d766829ea3f30f0c79e39719d77bac5e7fa0f
vietng <vietng322611@gmail.com>
2026-05-27 11:06:02 +0700

    use dash in urls

--- employees/urls.py
+++ employees/urls.py
@@ -8,8 +8,8 @@ urlpatterns = [
     path("user_create/", views.user_create, name="user_create"),
     path("savings/", views.manage_saving_plans, name="manage_saving_plans"),
     path("savings/<str:plan_id>/",views.manage_saving_plan_detail,name="manage_saving_plan_detail",),
-    path("saving_types/", views.manage_saving_types, name="manage_saving_types"),
-    path("saving_types/<int:saving_type_id>/", views.manage_saving_type_detail, name="manage_saving_type_detail"),
+    path("saving-types/", views.manage_saving_types, name="manage_saving_types"),
+    path("saving-types/<int:saving_type_id>/", views.manage_saving_type_detail, name="manage_saving_type_detail"),
     path("transactions/", views.manage_transactions, name="manage_transactions"),
     path("reports/", views.manage_reports, name="manage_reports"),
 ]
